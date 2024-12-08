fc7379c572f3e84623d223f090bc7b8a5986d59a
yunhwan <yunhwan.choo00@gmail.com>
2024-12-08 18:18:12 +0900

    Initial commit

---
+++ src/test/java/com/turtlesoup/backend/TurtlesoupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turtlesoup.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurtlesoupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
